031f84b2c187b446ddf9c2682e6553ad23f4fd21
Adam Sayer <asayer82@icloud.com>
2024-09-04 13:05:26 +1000

    Initial Commit

---
+++ iDice/iDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDiceApp.swift
+//  iDice
+//
+//  Created by Adam on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
