ac58442c1f9488cbce21c49e320f7a0ea2266ad7
Adam Sayer <asayer82@icloud.com>
2024-09-04 22:46:19 +1000

    MADE MVVM architecture

---
+++ iDice/ContentViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewModel.swift
+//  iDice
+//
+//  Created by Adam on 4/9/2024.
+//
+
+import SwiftUI
+
+struct ContentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewModel()
+}
